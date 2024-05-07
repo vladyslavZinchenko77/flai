ff377ad8a71db42e86ac26aa36f26c9d17f97f7e
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-05-07 12:46:45 +0300

    refactored keywords in mainpage

--- src/pages/GeneralPage/components/GrowthSection/GrowthSection.jsx
+++ src/pages/GeneralPage/components/GrowthSection/GrowthSection.jsx
@@ -10,26 +10,25 @@ const SectionContainer = styled.div`
   margin-top: 56px;
 `
 const GrowthHeader = styled.div`
-display: flex;
-justify-content: center;
+  display: flex;
+  justify-content: center;
 `
 const GrowthHeaderItem = styled.div`
-display: flex;
-margin-right: 5px;
-align-items:center;
-border: 1px solid grey;
-border-radius: 20px;
-
+  display: flex;
+  margin-right: 5px;
+  align-items: center;
+  border: 1px solid grey;
+  border-radius: 20px;
 `
 
 const IconContainer = styled.div`
-display: flex;
-justify-content: center;
-align-items:center;
-padding: 9px 14px;
-background: ${props => props.background};
-border-radius: 20px;
-z-index: 5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 9px 14px;
+  background: ${(props) => props.background};
+  border-radius: 20px;
+  z-index: 5;
 `
 
 const GrowthSection = () => {
@@ -39,29 +38,40 @@ const GrowthSection = () => {
         <span>Growth</span>
       </SectionTitle>
       <GrowthHeader>
-      <GrowthHeaderItem>
-        
-        <IconContainer style={{padding: '11px 18px'}} background="linear-gradient(180.00deg, rgb(255, 152, 251),rgb(255, 231, 105)),rgb(255, 255, 255)" >
-          <SvgIcon name={'like'} />
-        </IconContainer >
-        
-       
-        <CustomText margin="0 20px 0 10px" color='#FFFFFF' fontsize='12px'>High Trending Chance</CustomText>
-      </GrowthHeaderItem >
+        <GrowthHeaderItem>
+          <IconContainer
+            style={{ padding: '11px 18px' }}
+            background="linear-gradient(180.00deg, rgb(255, 152, 251),rgb(255, 231, 105)),rgb(255, 255, 255)"
+          >
+            <SvgIcon name={'like'} />
+          </IconContainer>
 
-      <GrowthHeaderItem>
-        <IconContainer background='#00FF94'>
-          <SvgIcon name={'constellation'} /> <CustomText fontsize='20px' fontWeight="bold">8</CustomText>
-        </IconContainer>
-        <CustomText margin="0 20px 0 10px" color='#FFFFFF' fontsize='12px'>Sound assessment from our AI</CustomText>
-      </GrowthHeaderItem>
+          <CustomText margin="0 20px 0 10px" color="#FFFFFF" fontsize="12px">
+            High Trending Chance
+          </CustomText>
+        </GrowthHeaderItem>
 
-      <GrowthHeaderItem >
-        <IconContainer background='#FFE600'>
-          <SvgIcon name={'geolocation'} /> <CustomText fontsize="20px">25%</CustomText>
-        </IconContainer>
-        <CustomText margin="0 20px 0 10px" color='#FFFFFF' fontsize='12px'>Geo Popularity</CustomText>
-      </GrowthHeaderItem>
+        <GrowthHeaderItem>
+          <IconContainer background="#00FF94">
+            <SvgIcon name={'constellation'} />{' '}
+            <CustomText fontsize="20px" fontWeight="bold">
+              8
+            </CustomText>
+          </IconContainer>
+          <CustomText margin="0 20px 0 10px" color="#FFFFFF" fontsize="12px">
+            Sound assessment from our AI
+          </CustomText>
+        </GrowthHeaderItem>
+
+        <GrowthHeaderItem>
+          <IconContainer background="#FFE600">
+            <SvgIcon name={'geolocation'} />{' '}
+            <CustomText fontsize="20px">25%</CustomText>
+          </IconContainer>
+          <CustomText margin="0 20px 0 10px" color="#FFFFFF" fontsize="12px">
+            Geo Popularity
+          </CustomText>
+        </GrowthHeaderItem>
       </GrowthHeader>
       <Chart />
     </SectionContainer>
--- src/pages/GeneralPage/components/MainInfoSection/components/Keywords.jsx
+++ src/pages/GeneralPage/components/MainInfoSection/components/Keywords.jsx
@@ -1,58 +1,33 @@
 import { Tag } from 'antd'
 import CustomText from '../../../../../components/Common/CustomText/CustomText'
 import Flex from '../../../../../components/Common/Flex/Flex'
+import { useUserStore } from '../../../../../store/store'
 
 const Keywords = () => {
+  const { user } = useUserStore(); 
+
+  const keywords = user.songs.length > 0 ? user.songs[0].keywords : [];
+
   return (
     <Flex>
       <CustomText fontSize="12px" color="#8E8F92">
         Keywords:
       </CustomText>
       <Flex style={{ marginLeft: 10 }}>
-        <Tag
-          style={{
-            color: '#FFFFFF',
-            backgroundColor: 'rgb(33, 33, 34)',
-            border: 'none',
-            padding: '5px 16px',
-            borderRadius: '20px',
-          }}
-        >
-          Whispering breeze
-        </Tag>
-        <Tag
-          style={{
-            color: '#FFFFFF',
-            backgroundColor: 'rgb(33, 33, 34)',
-            border: 'none',
-            padding: '5px 16px',
-            borderRadius: '20px',
-          }}
-        >
-          Wandering soul
-        </Tag>
-        <Tag
-          style={{
-            color: '#FFFFFF',
-            backgroundColor: 'rgb(33, 33, 34)',
-            border: 'none',
-            padding: '5px 16px',
-            borderRadius: '20px',
-          }}
-        >
-          Crimson dawn
-        </Tag>
-        <Tag
-          style={{
-            color: '#FFFFFF',
-            backgroundColor: 'rgb(33, 33, 34)',
-            border: 'none',
-            padding: '5px 16px',
-            borderRadius: '20px',
-          }}
-        >
-          Echoes of silence
-        </Tag>
+        {keywords.map((keyword, index) => (
+          <Tag
+            key={index}
+            style={{
+              color: '#FFFFFF',
+              backgroundColor: 'rgb(33, 33, 34)',
+              border: 'none',
+              padding: '5px 16px',
+              borderRadius: '20px',
+            }}
+          >
+            {keyword}
+          </Tag>
+        ))}
       </Flex>
     </Flex>
   )
