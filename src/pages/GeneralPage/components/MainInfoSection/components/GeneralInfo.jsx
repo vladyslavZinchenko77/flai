9061ef69bc2f294321d554ead6dd7afb7140b21c
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-05-07 12:51:38 +0300

    refactored a user information

--- src/pages/GeneralPage/components/MainInfoSection/components/GeneralInfo.jsx
+++ src/pages/GeneralPage/components/MainInfoSection/components/GeneralInfo.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import Flex from '../../../../../components/Common/Flex/Flex'
 import CustomText from '../../../../../components/Common/CustomText/CustomText'
 import SvgIcon from '../../../../../components/Common/SvgIcon/SvgIcon'
+import { useUserStore } from '../../../../../store/store'
 
 const Preview = styled.div`
   margin-right: 20px;
@@ -15,6 +16,11 @@ const Preview = styled.div`
 `
 
 const GeneralInfo = () => {
+  const { user } = useUserStore(); 
+
+
+  const song = user.songs.length > 0 ? user.songs[0] : null;
+
   return (
     <Flex>
       <Preview>
@@ -23,18 +29,18 @@ const GeneralInfo = () => {
       <div>
         <Flex>
           <CustomText fontSize="12px" color="#8E8F92">
-            Official Artist{' '}
+            {user.isOfficial ? 'Official Artist' : 'Artist'}
+            {user.isOfficial && <SvgIcon name="info" margin="0 0 0 5px" />}
           </CustomText>
-          <SvgIcon name="info" margin="0 0 0 5px" />
         </Flex>
         <CustomText fontSize="18px" margin="3px 0 0" color="#FFFFFF">
-          Khabane Lame
+          {song ? song.title : ''}
         </CustomText>
         <CustomText margin="6px 0 0" fontSize="14px" color="#ffffff">
-          Melodic sound
+          {song ? song.genre : ''}
         </CustomText>
         <CustomText margin="8px 0 0" fontSize="12px" color="#8E8F92">
-          Type:original
+          Type: {song ? song.type : ''}
         </CustomText>
         <Flex style={{ marginTop: 8 }}>
           <SvgIcon name={'spotify'} />
